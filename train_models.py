2317b0180be154c31d068a1367975dd2f6076c31
Isaac Rehg <isaacrehg@gmail.com>
2020-07-16 14:22:48 -0400

    refactor train and test methods to be callable without args global var

--- train_models.py
+++ train_models.py
@@ -14,14 +14,16 @@ parser.add_argument('--seed', type=int, default=1, help='seed for random number 
 parser.add_argument('--init-model-path', type=str, help='path to save file of the initialized model before training')
 parser.add_argument('--final-model-path', type=str, help='path to save file of the final model')
 parser.add_argument('--pretrained', action='store_true', help='start from pretrained initialization')
-parser.add_argument('--data-dir', type=str, default='../../data', help='path to data directory')
-parser.add_argument('--save-acc', action='store_true', help='save per class accuracies of the model after each epoch')
-parser.add_argument('--acc-save-path', type=str, default='models/accuracies.npz')
 
-# Training args
-parser.add_argument('--dataset', type=str, default='CIFAR', help='the dataset to train on')
+# Data args
+parser.add_argument('--data-dir', type=str, default='../../data', help='path to data directory')
+parser.add_argument('--dataset', type=str, default='CIFAR', choices=['CIFAR'], help='the dataset to train on')
 parser.add_argument('--batch-size-train', type=int, default=100, help='batch size for training')
 parser.add_argument('--batch-size-test', type=int, default=100, help='batch size for testing')
+
+# Training args
+parser.add_argument('--save-acc', action='store_true', help='save per class accuracies of the model after each epoch')
+parser.add_argument('--acc-save-path', type=str, default='models/accuracies.npz')
 parser.add_argument('--lr', type=float, default=0.005, help='learning rate for training')
 parser.add_argument('--epoch', type=int, default=100, help='number of epochs to train for')
 parser.add_argument('--num-classes', type=int, default=100, help='number of classes to train/test on')
@@ -33,14 +35,16 @@ def save_model(model_path):
     torch.save(model.state_dict(), model_path)
 
 
-def test(device=0):
+def test(model, loader, device=0):
     model.eval()
-    total, correct = np.zeros(args.num_classes), np.zeros(args.num_classes)
-    class_idxs = np.arange(args.num_classes)[None].repeat(args.batch_size_test, axis=0)
-    for i, x, y in tqdm(test_loader):
+    num_classes = model.fc.out_features
+    total, correct = np.zeros(num_classes), np.zeros(model.fc.out_features)
+    class_idxs = np.arange(num_classes)[None].repeat(loader.batch_size, axis=0)
+    for i, x, y in tqdm(loader):
         x, y = x.to(device), y.to(device)
         out = model(x)
         pred = out.argmax(dim=1)
+        # TODO debug
         y, pred = (y.cpu().numpy()[:, None] == class_idxs), (pred.cpu().numpy()[:, None] == class_idxs)
         total += y.sum(axis=0)
         correct += np.logical_and(pred, y).sum(axis=0)
@@ -49,12 +53,20 @@ def test(device=0):
     return correct, total
 
 
-def train(epochs, device=0):
+def train(model, train_loader, test_loader,
+          epochs=100,
+          lr=0.005,
+          save_acc=False,
+          acc_save_path=None,
+          model_save_path='final-model.pth',
+          device=0,
+          seed=None):
     model.train()
-    optim = torch.optim.Adam(model.parameters(), lr=args.lr)
+    optim = torch.optim.Adam(model.parameters(), lr=lr)
     loss_fn = torch.nn.CrossEntropyLoss()
     total, correct = [], []
-    torch.manual_seed(args.seed)  # seed dataloader shuffling
+    if seed:
+        torch.manual_seed(seed)  # seed dataloader shuffling
     for e in range(epochs):
         print('Beginning epoch %d/%d' % (e + 1, epochs))
         losses = []
@@ -66,14 +78,15 @@ def train(epochs, device=0):
             optim.step()
             optim.zero_grad()
             losses += [loss.item()]
+            break
         print('Mean loss for epoch %d: %.4f' % (e, sum(losses) / len(losses)))
         print('Test accuracy for epoch %d:' % e, end=' ')
-        total_, correct_ = test(device=device)
+        correct_, total_ = test(model, test_loader, device=device)
         total += [total_]
         correct += [correct_]
-        if args.save_acc:
-            np.savez(args.acc_save_path, correct=np.stack(correct, axis=0), total=np.stack(total, axis=0))
-        save_model(args.final_model_path)
+        if save_acc:
+            np.savez(acc_save_path, correct=np.stack(correct, axis=0), total=np.stack(total, axis=0))
+        save_model(model_save_path)
 
 
 # load network
@@ -95,4 +108,11 @@ if args.dataset == 'CIFAR':
                                        train=False)
 
 if __name__ == '__main__':
-    train(args.epoch, device=0)
+    train(model, train_loader, test_loader,
+          epochs=args.epoch,
+          lr=args.lr,
+          save_acc=args.save_acc,
+          acc_save_path=args.acc_save_path,
+          model_save_path=args.final_model_path,
+          device=0,
+          seed=args.seed)
